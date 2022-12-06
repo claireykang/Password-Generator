919902282395b86c1b1a9e0ddadb2096ba3d0578
claireykang <clairekang0113@gmail.com>
2022-12-06 13:04:51 -0800

    bug fixes and css

--- script.js
+++ script.js
@@ -12,29 +12,55 @@ function generate(){
     length = parseInt(prompt("Enter how many characters you want."));
     if(length<8 || length>50){
         alert("Your password should be between 8 and 50 characters!");
+        return;
     }
 
     var lowerCasePrompt = confirm("Do you want lowercase letters?");
-var upperCasePrompt = confirm("Do you want uppercase characters?");
-var numbersPrompt = confirm("Do you want numbers?");
-var specialCharsPrompt = confirm("Do you want special characters?");
-
     if (lowerCasePrompt) {
         possibilities += lowerCase
+    }else{
+        for (let index = 0; index < length; index++) {
+            password += possibilities[Math.floor(Math.random() * possibilities.length)];
+            console.log(password)
+            document.querySelector("#password").value = password;
+
+        }
+        return password;
     }
+    var upperCasePrompt = confirm("Do you want uppercase characters?");
     if(upperCasePrompt){
         possibilities += upperCase
     }
+    else{
+        for (let index = 0; index < length; index++) {
+            password += possibilities[Math.floor(Math.random() * possibilities.length)];
+            console.log(password)
+            document.querySelector("#password").value = password;
+        }
+        return password;
+    }
+
+    var numbersPrompt = confirm("Do you want numbers?");
     if(numbersPrompt){
         possibilities += numbers
+    }else{
+        for (let index = 0; index < length; index++) {
+            password += possibilities[Math.floor(Math.random() * possibilities.length)];
+            console.log(password)
+            document.querySelector("#password").value = password;
+
+        }
+        return password;
     }
+
+    var specialCharsPrompt = confirm("Do you want special characters?");
     if(specialCharsPrompt){
         possibilities += specialChars
     }
-
     for (let index = 0; index < length; index++) {
         password += possibilities[Math.floor(Math.random() * possibilities.length)];
         console.log(password)
     }
+
     document.querySelector("#password").value = password;
 }
